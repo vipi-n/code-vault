29b8fac0d091d34e2a5311453c60a96c3cfbbdde
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-03-11 18:13:27 +0530

    Update NumberOfSubstrings.java

--- src/practice/imp/NumberOfSubstrings.java
+++ src/practice/imp/NumberOfSubstrings.java
@@ -42,4 +42,23 @@ class NumberOfSubstrings {
         }
         return count;
     }
+  // optimized
+      public int numberOfSubstrings(String s) {
+        
+        int len = s.length();
+        int count = 0;
+        int[] temp = new int[]{-1, -1, -1};
+
+
+        for(int i = 0; i < len; i++) {
+                
+                temp[s.charAt(i) - 'a'] = i;
+                if(temp[0] != -1 && temp[1] != -1 && temp[2] != -1) {
+                    // Math.min(temp[0], temp[1], temp[2]) We need to check the min in these three, since in java we cannot compare them at once we are doing it 2 at a time
+                   int tempMin = Math.min(temp[0], temp[1]);
+                   count += 1 + Math.min(tempMin, temp[2]);
+            }
+        }
+        return count;
+    }
 }
