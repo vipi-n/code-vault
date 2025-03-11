501302b29f8c15b07a277200969563e8dab5a71a
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-03-11 11:41:47 +0530

    Update LengthOfLongestSubstring.java

--- src/practice/imp/LengthOfLongestSubstring.java
+++ src/practice/imp/LengthOfLongestSubstring.java
@@ -1,1 +1,23 @@
+class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
 
+       HashMap<Character, Integer> map = new HashMap<>();
+       int maxLen = Integer.MIN_VALUE;
+       int len = s.length();
+       int j = 0;
+
+       if(s.length() <= 0) {
+        return 0;
+       }
+
+       for(int i = 0; i < len; i++) {
+        
+        if(map.containsKey(s.charAt(i))) {
+            j = Math.max(j, map.get(s.charAt(i)) + 1);
+        }
+        map.put(s.charAt(i), i);
+        maxLen = Math.max(maxLen, i - j + 1);
+       } 
+       return maxLen;
+    }
+}
