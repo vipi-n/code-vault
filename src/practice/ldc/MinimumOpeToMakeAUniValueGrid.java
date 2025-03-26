bc5d84908c710404ceb26b4b7facc549fb8ac834
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-03-27 01:05:46 +0530

    Update MinimumOpeToMakeAUniValueGrid.java

--- src/practice/ldc/MinimumOpeToMakeAUniValueGrid.java
+++ src/practice/ldc/MinimumOpeToMakeAUniValueGrid.java
@@ -29,4 +29,31 @@ class MinimumOpeToMakeAUniValueGrid {
         }
         return minOp;
     }
+
+  //
+      public int minOperations_2(int[][] grid, int x) {
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+    
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+              arr.add(grid[i][j]);   
+            }
+        }
+        Collections.sort(arr);
+        int median = arr.get(arr.size() / 2);
+        int rem = arr.get(0) % x;
+        int minOp = 0;
+
+        for(int i = 0; i < arr.size(); i++) {
+
+            if(arr.get(i) % x != rem) {
+                return -1;
+            } else {
+                int count = Math.abs(arr.get(i) - median) / x;
+                minOp += count;
+            }
+        }
+        return minOp;
+    }
 }
