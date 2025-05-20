d41ee10c4b95101dfa7d977f0241c7669142418b
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-20 18:45:50 +0530

    Create TwoSumInputArraySorted.java

---
+++ src/practice/neetcode150/TwoSumInputArraySorted.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(numbers[left] + numbers[right] != target) {
+             
+             if(numbers[left] + numbers[right] < target  ) {
+                left++;
+             } else  {
+                right--;
+             }
+        }
+        return new int[]{left + 1,right + 1};    
+    }
+}
