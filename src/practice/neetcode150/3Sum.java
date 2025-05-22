8b2c75257da182705a46d8a8f3c46a37dcfdd149
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-23 01:25:46 +0530

    Update 3Sum.java

--- src/practice/neetcode150/3Sum.java
+++ src/practice/neetcode150/3Sum.java
@@ -30,4 +30,40 @@ class Solution {
       res.addAll(set);
       return res;  
     }
+
+    // no space
+    public List<List<Integer>> threeSumWithoutSpace(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int target = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
