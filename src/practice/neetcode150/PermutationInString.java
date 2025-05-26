a7abf05b81ebdc55a55b89744b86b2448a0492dc
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-27 01:12:26 +0530

    Update PermutationInString.java

--- src/practice/neetcode150/PermutationInString.java
+++ src/practice/neetcode150/PermutationInString.java
@@ -33,4 +33,35 @@ class Solution {
         return false;
         
     }
+
+    // optimized
+
+    
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int s1Len = s1.length();
+        int s2Len = s2.length();
+
+        if (s1Len > s2Len) return false;
+
+        int[] arr1 = new int[26]; // frequency of s1
+        int[] arr2 = new int[26]; // frequency of current window in s2
+
+        for (int i = 0; i < s1Len; i++) {
+            arr1[s1.charAt(i) - 'a']++;
+            arr2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(arr1, arr2)) return true;
+
+        for (int i = s1Len; i < s2Len; i++) {
+            arr2[s2.charAt(i) - 'a']++;                          // add new char to window
+            arr2[s2.charAt(i - s1Len) - 'a']--;                  // remove old char from window
+
+            if (Arrays.equals(arr1, arr2)) return true;
+        }
+
+        return false;
+    }
+}
 }
