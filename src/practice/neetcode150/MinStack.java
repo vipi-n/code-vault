ea2179ab0cde399283cadeb4303bf8a88ee2d3af
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-29 02:13:39 +0530

    Create MinStack.java

---
+++ src/practice/neetcode150/MinStack.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+        int top = stack.pop();
+        if (top == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+
