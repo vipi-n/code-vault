2288dcda8acb74c82c33fc4d311f0568accebaaa
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-30 00:57:45 +0530

    Create  DailyTemperatures.java

---
+++ src/practice/neetcode150/DailyTemperatures.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/daily-temperatures/description/
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+      Stack<Integer> stack = new Stack();
+      int[] result = new int[temperatures.length];
+
+      for(int i = 0; i < temperatures.length; i++) {
+          while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+
+            result[stack.peek()] = i - stack.pop();
+          }
+          stack.push(i);
+      } 
+      return result; 
+    }
+}
