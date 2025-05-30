0a2e291e78a9ab25efba77a63648965df7e147c7
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-31 02:27:55 +0530

    Create TimeBasedKey-ValueStore.java

---
+++ src/practice/neetcode150/TimeBasedKey-ValueStore.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/time-based-key-value-store/description/
+
+class TimeMap {
+
+    HashMap<String, TreeMap<Integer, String>> map;
+    public TimeMap() {
+       map = new HashMap<>(); 
+    }
+    
+    public void set(String key, String value, int timestamp) {
+
+        if(!map.containsKey(key)) {
+            map.put(key, new TreeMap<>());
+        }
+        map.get(key).put(timestamp, value);
+        
+    }
+    
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if(treeMap == null) return "";
+
+        Integer floor = treeMap.floorKey(timestamp);
+        if(floor == null) {
+            return "";
+        }
+        return treeMap.get(floor);
+
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
