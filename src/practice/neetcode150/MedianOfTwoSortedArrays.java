92afe7ccd5392b349e2e91a253f3cef9308cc877
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-31 02:46:48 +0530

    Update MedianOfTwoSortedArrays.java

--- src/practice/neetcode150/MedianOfTwoSortedArrays.java
+++ src/practice/neetcode150/MedianOfTwoSortedArrays.java
@@ -1,2 +1,27 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
+// brute-force
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int len = nums1.length + nums2.length;
+        int[] res = new int[len];
+        int j = 0;
+
+        for(int i = 0; i < len; i++) {   
+            if(i < nums1.length)res[i] = nums1[i];
+            else {
+                res[i] = nums2[j++];
+            }
+        }
+        
+        Arrays.sort(res);
+        if(len % 2 == 0) {
+           return (res[len / 2] + res[(len / 2) - 1]) / 2.0;
+        } else {
+            return res[len / 2];
+        }
+    }
+}
+
