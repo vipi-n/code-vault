6ed83291439cb9cb352f7c61d3c885bff63bd1d7
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-31 02:00:23 +0530

    Create SearchInRotatedSortedArray.java

---
+++ src/practice/neetcode150/SearchInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if(target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
