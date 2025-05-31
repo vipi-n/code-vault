987b73dd481c2da7bcd0116030d06b58ffb635d4
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-05-31 16:37:06 +0530

    Create  MergeTwoSortedLists.java

---
+++ src/practice/neetcode150/MergeTwoSortedLists.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummyNode = new ListNode(0);
+        ListNode currNode = dummyNode;
+
+        while(list1 != null && list2 != null) {
+
+            if(list1.val <= list2.val) {
+                currNode.next = list1;
+                list1 = list1.next;
+            } else{
+                currNode.next = list2;
+                list2 = list2.next;
+            }
+            currNode = currNode.next;
+        }
+
+        if(list1 != null) {
+            currNode.next = list1;   
+        }
+        if(list2 != null) {
+            currNode.next = list2;  
+        }
+        return dummyNode.next;   
+    }
+}
