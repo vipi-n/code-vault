f0d56c8053a9feed0d30e1ef789d51dafb1401d5
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-05 01:28:01 +0530

    Create BFS.java

---
+++ src/practice/tree/traversal/BFS.java
@@ -0,0 +1,44 @@
+//
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public static void main(String[] args) {
+
+        Node node = new Node(1);
+        node.left = new Node(2);
+        node.right = new Node(3);
+        node.right.left = new Node(7);
+        node.left.left = new Node(4);
+        printLevel(node);
+
+    }
+
+    public static void printLevel(Node root) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            Node curr = queue.poll();
+            System.out.println(curr.key); // 2, 3, 4, 7
+            if (curr.left != null) {
+                queue.add(curr.left);
+            }
+            if (curr.right != null) {
+                queue.add(curr.right);
+            }
+        }
+    }
+}
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    Node (int key) {
+        this.key = key;
+    }
+}
