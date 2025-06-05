31377cb59851e8589d7f68dcc39a9d28634a07a3
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-05 16:56:41 +0530

    Create RootEqualsSumOfChildren.java

---
+++ src/practice/tree/RootEqualsSumOfChildren.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/root-equals-sum-of-children/
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+
+        if(root == null || root.left == null || root.right== null) return false;
+
+        if(root.val == root.left.val + root.right.val) {
+            return true;
+        }
+
+        return checkTree(root.left) && checkTree(root.right);
+    }
+}
