fbcd9f8183907e01224ce09b25d28b42c4afa7c7
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-05 12:32:26 +0530

    Update BinaryTreeLevelOrderTraversal.java

--- src/practice/tree/BinaryTreeLevelOrderTraversal.java
+++ src/practice/tree/BinaryTreeLevelOrderTraversal.java
@@ -49,4 +49,39 @@ class BinaryTreeLevelOrderTraversal {
         return result;
         
     }
+
+    //Optimized
+
+    public List<List<Integer>> levelOrderOp(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+
+            result.add(temp);
+        }
+
+        return result;
+    }
 }
