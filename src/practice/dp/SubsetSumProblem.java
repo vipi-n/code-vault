7d58be178504ad39e8a2e049510a39b39cf9d003
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-14 00:40:03 +0530

    Update SubsetSumProblem.java

--- src/practice/dp/SubsetSumProblem.java
+++ src/practice/dp/SubsetSumProblem.java
@@ -22,4 +22,43 @@ class Solution {
         
         return notTake || take;
     }
+
+    // Memoization
+
+        static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n = arr.length;
+        int[][] dp = new int[n + 1][sum + 1];
+        
+         for (int row[] : dp)
+            Arrays.fill(row, -1);
+        
+        return isSubset(arr, sum, n - 1, dp);
+        
+    }
+    
+    private static boolean isSubset(int arr[], int sum, int n, int[][] dp) {
+        
+        if(sum == 0){
+            return true;
+        }
+        
+        if(n == 0) {
+            return arr[n] == sum;
+        }
+        
+        if(dp[n][sum] != -1) {
+            return dp[n][sum] != 0 ? true : false;
+        }
+        
+        boolean notTake = isSubset(arr, sum, n - 1, dp);
+        boolean take = false;
+        if (arr[n] <= sum)
+            take = isSubset(arr, sum - arr[n], n - 1, dp);
+            
+        dp[n][sum] =  notTake || take == true ? 1 : 0;   
+        
+        return notTake || take;
+        
+    }
 }
