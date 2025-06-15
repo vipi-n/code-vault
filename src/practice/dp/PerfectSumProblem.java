32e1cc600e798ec1857cd1be18921059f7a4c568
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-15 23:06:39 +0530

    Create PerfectSumProblem.java

---
+++ src/practice/dp/PerfectSumProblem.java
@@ -0,0 +1,24 @@
+//https://www.geeksforgeeks.org/problems/perfect-sum-problem5633/1
+
+class Solution {
+    public int perfectSum(int[] nums, int target) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][target + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1; // Empty subset sums to 0
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
