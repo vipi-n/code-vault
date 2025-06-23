1550b8fab58d87e7bc09ad39829ec7e72c3da417
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-24 01:37:54 +0530

    Update LongestCommonSubsequence.java

--- src/practice/dp/LongestCommonSubsequence.java
+++ src/practice/dp/LongestCommonSubsequence.java
@@ -55,4 +55,36 @@ class LongestCommonSubsequence {
                 lcs(text1, len1, text2, len2 - 1, dp));
         }
     }
+
+    // tabulation - bottom up
+        public int longestCommonSubsequence_tabulation(String text1, String text2) {
+        
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        
+        // initialization to 0 not needed in java
+        for(int i = 0; i <= len1; i++) {
+             dp[i][0] = 0;
+        }
+        for(int j = 0; j <= len2; j++) {
+                dp[0][j] = 0;
+         }
+        return lcs(text1, len1, text2, len2, dp);
+    }
+
+    private int lcs_tabulation(String text1, int len1, String text2, int len2, int[][] dp) {
+
+         for(int i = 1; i <= len1; i++) {
+             for(int j = 1; j <= len2; j++) {
+               
+            if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+         }
+        }
+        return dp[len1][len2];
+    }
 }
