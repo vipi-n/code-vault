aa3bbc37c07e8ff67d062b079a1ba40bbf3f0150
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-24 17:10:11 +0530

    Create PrintShortestCommonSupersequence.java

---
+++ src/practice/dp/PrintShortestCommonSupersequence.java
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/shortest-common-supersequence/description/
+
+class PrintShortestCommonSupersequence {
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+       int m = str1.length();
+       int n = str2.length();
+       int[][] dp = new int[m + 1][n + 1];
+
+       int i = m;
+       int j = n;
+       StringBuilder str = new StringBuilder();
+
+       lcs(str1, str2, m, n, dp);
+
+       while(i > 0 && j > 0) {
+
+        if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+            str.append(str1.charAt(i - 1));
+            i--;
+            j--;
+        } else {
+            if(dp[i - 1][j] > dp[i][j - 1]) {
+                str.append(str1.charAt(i - 1));
+                i--;
+            } else {
+                str.append(str2.charAt(j - 1));
+                j--;
+            }
+        }
+       }
+
+       while(i > 0) {
+        str.append(str1.charAt(i - 1));
+        i--;
+       }
+        while(j > 0) {
+        str.append(str2.charAt(j - 1));
+        j--;
+       }
+
+       return str.reverse().toString();
+    }
+
+    private void lcs(String str1, String str2, int m, int n, int[][] dp) {
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+    }
+}
