72df29fdf2083feae32c3febb57218262ee918b7
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-06-27 01:43:51 +0530

    Update PalindromicPartitioning.java

--- src/practice/dp/PalindromicPartitioning.java
+++ src/practice/dp/PalindromicPartitioning.java
@@ -50,3 +50,56 @@ class PalindromicPartitioning {
        return true;
     }
 }
+
+// slightly optimized but still TLE :(
+
+// User function Template for Java
+
+class Solution {
+    
+    static int[][] dp;
+    static int palPartition(String s) {
+        
+        int n = s.length();
+        dp = new int[n + 1][n + 1];
+        
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(s, 0, n - 1);
+        
+    }
+    
+    private static int solve(String s, int i, int j) {
+       
+       if(i >= j || isPalindrome(s, i, j)) return 0;
+       
+       if(dp[i][j] != -1) return dp[i][j];
+       
+       int min = Integer.MAX_VALUE;
+     
+       
+       for(int k = i; k < j; k++) {
+           
+          int leftCut = dp[i][k] != -1 ?  dp[i][k] : solve(s, i, k);
+          int rightCut = dp[k + 1][j] != -1 ?  dp[k + 1][j] : solve(s, k + 1, j);
+          
+          int cuts = 1 + leftCut + rightCut;
+          min = Math.min(min, cuts);
+       }
+       dp[i][j] = min;
+       return dp[i][j];
+    }
+    
+    private static boolean isPalindrome(String s, int i, int j) {
+       
+       while(i <= j) {
+           if(s.charAt(i) != s.charAt(j)) {
+               return false;
+           }
+           i++;
+           j--;
+       }
+       return true;
+    }
+}
