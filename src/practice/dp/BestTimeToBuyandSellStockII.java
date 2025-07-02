e8c6d41662de153bf39158b98b0cd04ba520f17e
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-03 02:01:40 +0530

    Update BestTimeToBuyandSellStockII.java

--- src/practice/dp/BestTimeToBuyandSellStockII.java
+++ src/practice/dp/BestTimeToBuyandSellStockII.java
@@ -35,3 +35,56 @@ class Solution {
         return maxProfit;
     }
 }
+
+// 3. rec
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+        
+        return profit(0, prices, 1);
+    }
+
+    private int profit(int ind, int[] prices, int buy) {
+
+        if(ind == prices.length) return 0;
+        int profit = 0;
+        if(buy == 1) {
+            profit = Math.max(-prices[ind] + profit(ind + 1, prices, 0),
+                     profit(ind + 1, prices, 1));
+        } else {
+            profit = Math.max(prices[ind] + profit(ind + 1, prices, 1),
+                    profit(ind + 1, prices, 0));
+        }
+        return profit;
+    }
+}
+// 4. memo
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+        
+        int[][] dp = new int[prices.length][2];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return profit(0, prices, 1, dp);
+    }
+
+    private int profit(int ind, int[] prices, int buy, int[][] dp) {
+
+        if(ind == prices.length) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        int profit = 0;
+        
+        if(buy == 1) {
+            profit = Math.max(-prices[ind] + profit(ind + 1, prices, 0, dp),
+                     profit(ind + 1, prices, 1, dp));
+        } else {
+            profit = Math.max(prices[ind] + profit(ind + 1, prices, 1, dp),
+                    profit(ind + 1, prices, 0, dp));
+        }
+        dp[ind][buy] = profit;
+        return profit;
+    }
+}
+
