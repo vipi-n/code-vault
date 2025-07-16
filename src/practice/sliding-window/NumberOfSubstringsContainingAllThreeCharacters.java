231c147186f850090b63476eaf4ee1757e8fab20
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-17 00:27:09 +0530

    Update NumberOfSubstringsContainingAllThreeCharacters.java

--- src/practice/sliding-window/NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/practice/sliding-window/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -20,4 +20,41 @@ class NumberOfSubstringsContainingAllThreeCharacters {
         }
         return count;
     }
+
+    // another brute
+        public int numberOfSubstrings(String s) {
+        
+        int n = s.length();
+        int count = 0;
+        int[] temp;
+
+        for(int i = 0; i < n; i++) {
+            temp = new int[3];
+            for(int j = i; j < n; j++) {
+                temp[s.charAt(j) - 'a'] = 1;
+                if(temp[0] + temp[1] + temp[2] == 3) {
+                  count += n - j;
+                  break;
+                } 
+            }
+        }
+        return count;
+    }
+
+    // optimized
+        public int numberOfSubstrings(String s) {
+        
+        int n = s.length();
+        int count = 0;
+        int[] temp = new int[]{-1, -1, -1};
+
+        for(int i = 0; i < n; i++) {
+
+            temp[s.charAt(i) - 'a'] = i;
+                if(temp[0] != -1 && temp[1] != -1 && temp[2] != -1) {
+                  count += 1 + Math.min(temp[0], Math.min(temp[1], temp[2]));
+            } 
+        }
+        return count;
+    }
 }
