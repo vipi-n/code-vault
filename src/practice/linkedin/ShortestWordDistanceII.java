b46bb8b167f40532259ee31000fd6f6a7525b0af
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-24 00:54:03 +0530

    Update and rename ShortestDistance.java to ShortestWordDistanceII.java

--- src/practice/linkedin/ShortestWordDistanceII.java
+++ src/practice/linkedin/ShortestWordDistanceII.java
@@ -3,7 +3,7 @@
 
 import java.util.*;
 
-class ShortestDistance {
+class ShortestWordDistanceII {
     private Map<String, List<Integer>> wordIndexMap;
 
     public WordDistanceII(String[] wordsDict) {
