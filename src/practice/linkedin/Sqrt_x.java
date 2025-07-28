4770a7d59ab4f315322a2d21d753c779e028111f
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-29 00:43:28 +0530

    Create Sqrt_x.java

---
+++ src/practice/linkedin/Sqrt_x.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/sqrtx/
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) return mid;
+            else if (square < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
