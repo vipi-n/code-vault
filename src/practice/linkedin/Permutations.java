b42bf614c00947ab74777693d06d6832dce9795f
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-29 13:01:48 +0530

    Create Permutations.java

---
+++ src/practice/linkedin/Permutations.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/permutations/description/
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+       
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList, new ArrayList<>(), nums);
+        return resultList;
+    }
+
+    private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums) {
+        // If we match the length, it is a permutation
+        if (tempList.size() == nums.length) {
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int number : nums) {
+            // Skip if we get same element
+            if (tempList.contains(number))
+                continue;
+
+            // Add the new element
+            tempList.add(number);
+
+            // Go back to try other element
+            backtrack(resultList, tempList, nums);
+
+            // Remove the element
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
