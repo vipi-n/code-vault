b36f7abee27f9a37a52e827f99d95374d27cd9c6
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-07-31 03:03:52 +0530

    Create combinationSum3.java

---
+++ src/practice/recursion_backtrack/combinationSum3.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/combination-sum-iii/
+
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        getAllSubsetWithSumK(1, k, res, new ArrayList<>(), 1, 0, n);
+        return res;
+
+    }
+
+    private void getAllSubsetWithSumK(int num, int k, List<List<Integer>> res, List<Integer> list, int index, int sum,
+            int target) {
+
+        if (sum > target || list.size() > k || index > 10)
+            return;
+
+        if (list.size() == k) {
+            if (sum == target) {
+                res.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        list.add(num);
+        sum += num;
+        getAllSubsetWithSumK(num + 1, k, res, list, index + 1, sum, target);
+        sum -= num;
+        list.remove(list.size() - 1);
+        getAllSubsetWithSumK(num + 1, k, res, list, index + 1, sum, target);
+
+    }
+}
