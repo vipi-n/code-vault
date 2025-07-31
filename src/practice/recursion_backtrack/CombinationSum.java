3575287ada9a677efb239102eb63bbf0194dd330
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-08-01 01:16:31 +0530

    Update CombinationSum.java

--- src/practice/recursion_backtrack/CombinationSum.java
+++ src/practice/recursion_backtrack/CombinationSum.java
@@ -30,3 +30,32 @@ class Solution {
 
     }
 }
+
+// clean
+
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        getAllSubsetWithSumK(candidates, res, new ArrayList<>(), 0, target);
+        return res;
+    }
+
+    private void getAllSubsetWithSumK(int[] arr, List<List<Integer>> res, List<Integer> list, int index, int target) {
+
+        if (index == arr.length) {
+            if (target == 0) {
+                res.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        if (arr[index] <= target) { // if arr[index] is greater than sume we'll never find target 
+            list.add(arr[index]); // pick
+            getAllSubsetWithSumK(arr, res, list, index, target - arr[index]);
+            list.remove(list.size() - 1); // we need to remove what we have added for not-pick case
+        }
+        getAllSubsetWithSumK(arr, res, list, index + 1, target); // not pick, so target remains same
+
+    }
+}
