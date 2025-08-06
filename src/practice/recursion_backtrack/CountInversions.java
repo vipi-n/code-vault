14db78f1e3aadcfbbb915035db58964e8e53a092
Vipin <65813964+vipi-n@users.noreply.github.com>
2025-08-06 08:12:56 +0530

    Create CountInversions.java

---
+++ src/practice/recursion_backtrack/CountInversions.java
@@ -0,0 +1,59 @@
+// https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+
+class Solution {
+    static int inversionCount(int arr[]) {
+        int low = 0;
+        int high = arr.length - 1;
+        return mergeSortFunc(arr, low, high);   
+    }
+
+    public static int mergeSortFunc(int[] arr, int low, int high) {
+
+        int count = 0;
+        if(low >= high) return 0;
+        int mid = (low + high) / 2;
+
+        count += mergeSortFunc(arr, low, mid);
+        count += mergeSortFunc(arr, mid + 1, high);
+        count += merge(arr, low, mid, high);
+        return count;
+    }
+
+    private static int merge(int[] arr, int low, int mid, int high) {
+       
+        int count = 0;
+        int[] temp = new int[high - low + 1];
+        // [low, mid] + [mid + 1, high]
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+        while(left <= mid && right <= high) {
+            if(arr[left] <= arr[right]) {
+                temp[k] = arr[left];
+                k++;
+                left++;
+            } else {
+                count += (mid - left) + 1;
+                temp[k] = arr[right];
+                k++;
+                right++;
+            }
+        }
+
+        while(left <= mid) {
+            temp[k] = arr[left];
+            k++;
+            left++;
+        }
+        while(right <= high) {
+            temp[k] = arr[right];
+            k++;
+            right++;
+        }
+        for(int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
+        }
+        return count;
+        
+    }
+}
